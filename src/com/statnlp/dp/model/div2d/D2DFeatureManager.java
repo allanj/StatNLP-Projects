612fcd732dc82c2e7f90606fa91f99c7b98c03f8
allanjie <allanmcgrady@gmail.com>
2016-05-04 00:42:36 +0800

    d2d-2.0

--- src/com/statnlp/dp/model/div2d/D2DFeatureManager.java
+++ src/com/statnlp/dp/model/div2d/D2DFeatureManager.java
@@ -106,7 +106,7 @@ public class D2DFeatureManager extends FeatureManager {
 				throw new RuntimeException("parent is general span, but have "+children_k.length+" children?");
 
 			//leaf nodes.. local features: include the O features
-			if(!child_1_type.equals(OE) && leftIndex==rightIndex && !sent.get(leftIndex).getName().equals("ROOT")){
+			if(!child_1_type.equals(OE)  && leftIndex==rightIndex && !sent.get(leftIndex).getName().equals("ROOT")){
 				int i = leftIndex;
 				String word = sent.get(i).getName();
 				String tag = sent.get(i).getTag();
@@ -149,32 +149,31 @@ public class D2DFeatureManager extends FeatureManager {
 				
 				
 				/******************The begin of that entity*********************/
-				if( leftIndex!=rightIndex){
-					String word = sent.get(leftIndex).getName();
-					String tag = sent.get(leftIndex).getTag();
-					String prevWord = leftIndex>1?sent.get(leftIndex-1).getName():"STR";
-					String prevTag = leftIndex>1?sent.get(leftIndex-1).getTag():"STR";
-					String nextWord = leftIndex<sent.length()-1? sent.get(leftIndex+1).getName():"END";
-					String nextTag = leftIndex<sent.length()-1? sent.get(leftIndex+1).getTag():"END";
-					String child_type = E_B_PREFIX+child_1_type;
-					featureList.add(this._param_g.toFeature(network, FEATYPE.entity.name(), "B-EW", child_type+":"+word));
-					featureList.add(this._param_g.toFeature(network, FEATYPE.entity.name(), "B-ET", child_type+":"+tag));
-					featureList.add(this._param_g.toFeature(network, FEATYPE.entity.name(), "B-ELW", child_type+":"+prevWord));
-					featureList.add(this._param_g.toFeature(network, FEATYPE.entity.name(), "B-ELT", child_type+":"+prevTag));
-					featureList.add(this._param_g.toFeature(network, FEATYPE.entity.name(), "B-ERW", child_type+":"+nextWord));
-					featureList.add(this._param_g.toFeature(network, FEATYPE.entity.name(), "B-ERT", child_type+":"+nextTag));
-					featureList.add(this._param_g.toFeature(network, FEATYPE.entity.name(), "B-ELT-T",child_type+":"+prevTag+","+tag));
-					/****Add some prefix features******/
-					for(int plen = 1;plen<=6;plen++){
-						if(word.length()>=plen){
-							String suff = word.substring(word.length()-plen, word.length());
-							String pref = word.substring(0,plen);
-							featureList.add(this._param_g.toFeature(network,FEATYPE.entity.name(), "B-E-PATTERN-SUFF-"+plen, child_type+":"+suff));
-							featureList.add(this._param_g.toFeature(network,FEATYPE.entity.name(), "B-E-PATTERN-PREF-"+plen, child_type+":"+pref));
-						}
-					}
-					
-				}
+//				if( leftIndex!=rightIndex){
+//					String word = sent.get(leftIndex).getName();
+//					String tag = sent.get(leftIndex).getTag();
+//					String prevWord = leftIndex>1?sent.get(leftIndex-1).getName():"STR";
+//					String prevTag = leftIndex>1?sent.get(leftIndex-1).getTag():"STR";
+//					String nextWord = leftIndex<sent.length()-1? sent.get(leftIndex+1).getName():"END";
+//					String nextTag = leftIndex<sent.length()-1? sent.get(leftIndex+1).getTag():"END";
+//					String child_type = E_B_PREFIX+child_1_type;
+//					featureList.add(this._param_g.toFeature(network, FEATYPE.entity.name(), "B-EW", child_type+":"+word));
+//					featureList.add(this._param_g.toFeature(network, FEATYPE.entity.name(), "B-ET", child_type+":"+tag));
+//					featureList.add(this._param_g.toFeature(network, FEATYPE.entity.name(), "B-ELW", child_type+":"+prevWord));
+//					featureList.add(this._param_g.toFeature(network, FEATYPE.entity.name(), "B-ELT", child_type+":"+prevTag));
+//					featureList.add(this._param_g.toFeature(network, FEATYPE.entity.name(), "B-ERW", child_type+":"+nextWord));
+//					featureList.add(this._param_g.toFeature(network, FEATYPE.entity.name(), "B-ERT", child_type+":"+nextTag));
+//					featureList.add(this._param_g.toFeature(network, FEATYPE.entity.name(), "B-ELT-T",child_type+":"+prevTag+","+tag));
+//					/****Add some prefix features******/
+//					for(int plen = 1;plen<=6;plen++){
+//						if(word.length()>=plen){
+//							String suff = word.substring(word.length()-plen, word.length());
+//							String pref = word.substring(0,plen);
+//							featureList.add(this._param_g.toFeature(network,FEATYPE.entity.name(), "B-E-PATTERN-SUFF-"+plen, child_type+":"+suff));
+//							featureList.add(this._param_g.toFeature(network,FEATYPE.entity.name(), "B-E-PATTERN-PREF-"+plen, child_type+":"+pref));
+//						}
+//					}
+//				}
 			}
 			
 			addDepFeatures(featureList,network,parentArr,children_k,sent);
--- src/com/statnlp/dp/model/div2d/D2DInstance.java
+++ src/com/statnlp/dp/model/div2d/D2DInstance.java
@@ -101,62 +101,59 @@ public class D2DInstance extends DependInstance {
 	@Override
 	public String[] toEntities(Tree spanRoot){
 //		System.err.println(spanRoot.pennString());
-		
+		ArrayList<ArrayList<PositionType>> esArr = new ArrayList<ArrayList<PositionType>>();
+		for(int i=0;i<this.sentence.length();i++){
+			ArrayList<PositionType> list = new ArrayList<PositionType>();
+			esArr.add(list);
+		}
 		String[] es = new String[this.sentence.length()];
-		boolean[] set = new boolean[this.sentence.length()];
+		
+		String[] res = new String[this.sentence.length()];
 		es[0] = O_TYPE;
-		for(int i=1;i<es.length;i++) {es[i] = "UNDEF"; set[i] =false;}
-		this.findAllONE(es, spanRoot);
-		resCovered = false;
-		this.findAllE(es, spanRoot);
+		res[0] = es[0];
+		ArrayList<PositionType> rootPosType = new ArrayList<PositionType>();
+		rootPosType.add(new PositionType(O_TYPE, 1));
+		esArr.set(0, rootPosType);
+		this.findAllE(spanRoot, esArr);
 		for(int i=1;i<es.length;i++){
-			if(es[i].equals("UNDEF")) {
-				System.err.println("Some spans are not finalized the type. Info:\n\t"+this._instanceId+"\n"+this.getInput().toString());
-				es[i] = O_TYPE;
+			if(esArr.get(i).size()==0) es[i] = O_TYPE;
+			else if(esArr.get(i).size()==1) es[i] = esArr.get(i).get(0).type;
+			else if(esArr.get(i).size()==2){
+				es[i] = esArr.get(i).get(0).length >= esArr.get(i).get(1).length? esArr.get(i).get(0).type: esArr.get(i).get(1).type;
+			}else{
+				throw new RuntimeException("one word cannot have more two types");
 			}
-			if((es[i-1].startsWith(E_I_PREFIX) || es[i-1].startsWith(E_B_PREFIX)) && es[i].startsWith(E_B_PREFIX) && es[i-1].substring(2, es[i-1].length()).equals(es[i].substring(2, es[i].length())))
-				es[i] = E_I_PREFIX+es[i-1].substring(2, es[i-1].length());
-		}
-		return es;
-	}
-	
-	private void findAllONE(String[] es, Tree current){
-		CoreLabel label = (CoreLabel)(current.label());
-		String[] info = label.value().split(",");
-		int l = Integer.valueOf(info[0]);
-		int r = Integer.valueOf(info[1]);
-		String type = info[4];
-		if(type.equals(ONE)){
-			for(int i=l;i<=r;i++) es[i] = O_TYPE;
-			return;
-		}else if(!type.startsWith(PARENT_IS) && !type.equals(OE) && !type.equals(ONE)){
-			return;
-		}else{
-			for(Tree child: current.children()){
-				findAllONE(es,child);
+			if(!es[i-1].equals(es[i]) && !es[i].equals(O_TYPE)){
+				res[i] = E_B_PREFIX + es[i];
+			}else if(es[i-1].equals(es[i]) && !es[i].equals(O_TYPE)){
+				res[i] = E_I_PREFIX + es[i];
+			}else if(es[i].equals(O_TYPE)){
+				res[i] = O_TYPE;
 			}
 		}
+		
+		
+		return res;
 	}
 	
-	private void findAllE(String[] es, Tree current){
+	
+	private void findAllE(Tree current, ArrayList<ArrayList<PositionType>> esArr){
 		CoreLabel label = (CoreLabel)(current.label());
 		String[] info = label.value().split(",");
 		int l = Integer.valueOf(info[0]);
 		int r = Integer.valueOf(info[1]);
 		String type = info[4];
 		if(!type.startsWith(PARENT_IS) && !type.equals(OE) && !type.equals(ONE) ){
-			if((es[l].startsWith(E_B_PREFIX) || es[l].startsWith(E_I_PREFIX)) && !es[l].substring(2).equals(type)) resCovered = true;
-			es[l] = E_B_PREFIX+type;
-			for(int i=l+1;i<=r;i++){
-				if((es[i].startsWith(E_B_PREFIX) || es[i].startsWith(E_I_PREFIX)) && !es[i].substring(2).equals(type)) resCovered = true;
-				es[i]=E_I_PREFIX+type;
+			for(int i=l;i<=r;i++){
+				ArrayList<PositionType> posList = esArr.get(i);
+				posList.add(new PositionType(type, l-r+1));
 			}
 			return;
 		}else if(type.equals(ONE)){
 			return;
 		}else{
 			for(Tree child: current.children()){
-				findAllE(es,child);
+				findAllE(child,esArr);
 			}
 		}
 	}
