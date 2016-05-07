06d3333e93b5d60154712e97d53732009bd976d0
allanjie <allanmcgrady@gmail.com>
2016-05-07 23:35:04 +0800

    back to 3.2.2

--- src/com/statnlp/dp/model/div/DIVFeatureManager_leafcopy.java
+++ src/com/statnlp/dp/model/div/DIVFeatureManager_leafcopy.java
@@ -144,38 +144,6 @@ public class DIVFeatureManager_leafcopy extends FeatureManager {
 				}
 			}
 			
-			
-			if(!pa_type.equals(OE) && !pa_type.equals(ONE) && !child_1_type.equals(OE) && !child_1_type.equals(ONE) && leftIndex!=rightIndex){
-				//repeated unigram
-				for(int i=leftIndex;i<=rightIndex;i++){
-					String word = sent.get(i).getName();
-					String tag = sent.get(i).getTag();
-					String prevWord = i>1?sent.get(i-1).getName():"STR";
-					String prevTag = i>1?sent.get(i-1).getTag():"STR";
-					String nextWord = i<sent.length()-1? sent.get(i+1).getName():"END";
-					String nextTag = i<sent.length()-1? sent.get(i+1).getTag():"END";
-					String child_type = child_1_type;
-					featureList.add(this._param_g.toFeature(network, FEATYPE.entity.name(), "EW", child_type+":"+word));
-					featureList.add(this._param_g.toFeature(network, FEATYPE.entity.name(), "ET", child_type+":"+tag));
-					featureList.add(this._param_g.toFeature(network, FEATYPE.entity.name(), "ELW", child_type+":"+prevWord));
-					featureList.add(this._param_g.toFeature(network, FEATYPE.entity.name(), "ELT", child_type+":"+prevTag));
-					featureList.add(this._param_g.toFeature(network, FEATYPE.entity.name(), "ERW", child_type+":"+nextWord));
-					featureList.add(this._param_g.toFeature(network, FEATYPE.entity.name(), "ERT", child_type+":"+nextTag));
-					featureList.add(this._param_g.toFeature(network, FEATYPE.entity.name(), "ELT-T",child_type+":"+prevTag+","+tag));
-					for(int plen = 1;plen<=6;plen++){
-						if(word.length()>=plen){
-							String suff = word.substring(word.length()-plen, word.length());
-							String pref = word.substring(0,plen);
-							featureList.add(this._param_g.toFeature(network,FEATYPE.entity.name(), "E-PATTERN-SUFF-"+plen, child_type+":"+suff));
-							featureList.add(this._param_g.toFeature(network,FEATYPE.entity.name(), "E-PATTERN-SUFF-"+plen+"-dir", child_type+":"+suff+":"+att));
-							featureList.add(this._param_g.toFeature(network,FEATYPE.entity.name(), "E-PATTERN-PREF-"+plen, child_type+":"+pref));
-							featureList.add(this._param_g.toFeature(network,FEATYPE.entity.name(), "E-PATTERN-PREF-"+plen+"-dir", child_type+":"+pref+":"+att));
-						}
-					}
-				}
-				
-			}
-			
 			//the outmost entity
 			if((pa_type.equals(OE)||pa_type.equals("null")) && !child_1_type.equals(OE) && !child_1_type.equals(ONE)){
 				
@@ -218,6 +186,7 @@ public class DIVFeatureManager_leafcopy extends FeatureManager {
 				for(int i=leftIndex;i<=rightIndex;i++){
 					String word = sent.get(i).getName();
 					String tag = sent.get(i).getTag();
+					
 					String prevWord = i>1?sent.get(i-1).getName():"STR";
 					String prevTag = i>1?sent.get(i-1).getTag():"STR";
 					String nextWord = i<sent.length()-1? sent.get(i+1).getName():"END";
@@ -230,6 +199,7 @@ public class DIVFeatureManager_leafcopy extends FeatureManager {
 					featureList.add(this._param_g.toFeature(network, FEATYPE.entity.name(), "ERW", child_type+":"+nextWord));
 					featureList.add(this._param_g.toFeature(network, FEATYPE.entity.name(), "ERT", child_type+":"+nextTag));
 					featureList.add(this._param_g.toFeature(network, FEATYPE.entity.name(), "ELT-T",child_type+":"+prevTag+","+tag));
+					
 				}
 				
 				for(int i=leftIndex;i<=rightIndex-1;i++){
@@ -327,9 +297,6 @@ public class DIVFeatureManager_leafcopy extends FeatureManager {
 				}
 				featureList.add(this._param_g.toFeature(network,FEATYPE.entity.name(),"E-whole-words",child_1_type+":"+sb.toString()) );
 				featureList.add(this._param_g.toFeature(network,FEATYPE.entity.name(),"E-whole-tags",child_1_type+":"+sbTags.toString()) );
-				
-				
-				
 			}
 			
 			//joint features
