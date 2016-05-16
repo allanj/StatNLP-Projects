07d083b5d3bb0dcd5558553a148827ea546bb540
allanjie <allanmcgrady@gmail.com>
2016-05-16 20:37:01 +0800

    Temporary adds the readWrite weight

--- src/com/statnlp/hybridnetworks/NetworkModel.java
+++ src/com/statnlp/hybridnetworks/NetworkModel.java
@@ -16,11 +16,15 @@
  */
 package com.statnlp.hybridnetworks;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
@@ -30,7 +34,9 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+import com.statnlp.commons.crf.RAWF;
 import com.statnlp.commons.types.Instance;
+import com.statnlp.dp.utils.DPConfig;
 
 public abstract class NetworkModel implements Serializable{
 	
@@ -154,6 +160,51 @@ public abstract class NetworkModel implements Serializable{
 		
 		double obj_old = Double.NEGATIVE_INFINITY;
 		
+		//now read the weight
+				HashMap<String, Double> f2weight = null;
+				HashSet<String> ftypes = new HashSet<String>();
+				for(DPConfig.WEIGHT_TYPE t: DPConfig.WEIGHT_TYPE.values()){
+					ftypes.add(t.name());
+				}
+				if(DPConfig.readWeight){
+					try {
+						f2weight = new HashMap<String, Double>();
+						//HashSet<String> obtained = new HashSet<String>();
+						BufferedReader br = RAWF.reader(DPConfig.weightPath);
+						String line = null;
+						while((line = br.readLine())!=null){
+							String[] values = line.split("<MYSEP>");
+							f2weight.put(values[0], Double.parseDouble(values[1]));
+						}
+						br.close();
+						System.err.println("[Info] Total number of ecrf weight:"+f2weight.size());
+						int totalObtained = 0;
+						for(int tf=0;tf<this._fm._param_g._feature2rep.length;tf++){
+							String nowf = Arrays.toString(this._fm._param_g._feature2rep[tf]);
+							if(!f2weight.containsKey(nowf)){
+								System.err.println(nowf);
+								//this._fm.getParam_G().setWeight(tf, 0.0);
+							}else{
+								if(ftypes.contains(this._fm._param_g._feature2rep[tf][0])){
+									this._fm.getParam_G().overRideWeight(tf, f2weight.get(nowf));
+									totalObtained++;
+								}
+									
+								//f2weight.remove(nowf);
+							}
+						}
+						System.err.println("[Info] Total feature weight obtained from dep:"+totalObtained);
+						//Iterator<String> iter = f2weight.keySet().iterator();
+						//while(iter.hasNext()){
+						//	System.err.println(iter.next());
+						//}
+						
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+		
 		//run the EM-style algorithm now...
 		long startTime = System.currentTimeMillis();
 		try{
@@ -193,10 +244,36 @@ public abstract class NetworkModel implements Serializable{
 					System.out.println("Training completes. No significant progress (<objtol) after "+it+" iterations.");
 					break;
 				}
+				//everytime before start, read the weight because we fixed the weight:
+				if(DPConfig.readWeight){
+					for(int tf=0;tf<this._fm._param_g._feature2rep.length;tf++){
+						String nowf = Arrays.toString(this._fm._param_g._feature2rep[tf]);
+						if(!f2weight.containsKey(nowf) && ftypes.contains(this._fm._param_g._feature2rep[tf][0])){
+							
+						}else{
+							if(ftypes.contains(this._fm._param_g._feature2rep[tf][0]))
+								this._fm.getParam_G().overRideWeight(tf, f2weight.get(nowf));
+						}
+					}
+				}
 			}
 			//use the best weights 
 			if(NetworkConfig.USE_STRUCTURED_SVM){
 				this._fm.getParam_G().setBestWeights();
+			}
+			
+			if(DPConfig.writeWeight){
+				try {
+					PrintWriter pw = RAWF.writer(DPConfig.weightPath);
+					for(int tf=0;tf<this._fm._param_g._feature2rep.length;tf++){
+						//System.err.println(Arrays.toString(this._fm._param_g._feature2rep[tf])+"  feature weight:"+this._fm.getParam_G().getWeight(tf));
+						pw.write(Arrays.toString(this._fm._param_g._feature2rep[tf])+"<MYSEP>"+this._fm.getParam_G().getWeight(tf)+"\n");
+					}
+					pw.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		} finally {
 			pool.shutdown();
