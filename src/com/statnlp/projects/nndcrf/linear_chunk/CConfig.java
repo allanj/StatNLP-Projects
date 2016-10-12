968bd3f16639b917efbdb696d90c7ae7c5885081
allanjie <allanmcgrady@gmail.com>
2016-10-12 16:33:01 +0800

    just make the windows config to false

--- src/com/statnlp/projects/nndcrf/linear_chunk/CConfig.java
+++ src/com/statnlp/projects/nndcrf/linear_chunk/CConfig.java
@@ -2,6 +2,6 @@ package com.statnlp.projects.nndcrf.linear_chunk;
 
 public class CConfig {
 
-	public static boolean windows = true;
+	public static boolean windows = false;
 	public static String evalScript = "data/conlleval.pl";  //remember to make the script runnable
 }
