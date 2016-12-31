828eb3dcfe69e7f0e0985148f2f67622659fd316
allanj <allanmcgrady@gmail.com>
2016-12-31 18:51:07 +0800

    First version of the pipeline model

---
+++ src/com/statnlp/projects/dep/model/pipeline/Writer.java
@@ -0,0 +1,11 @@
+package com.statnlp.projects.dep.model.pipeline;
+
+import com.statnlp.commons.types.Instance;
+
+public class Writer {
+
+	public static void outputCoNLLX(Instance[] semiRes, String output) {
+		
+	}
+
+}
