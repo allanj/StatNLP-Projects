89dc6233e77a6a9ccffbc87a6dbb29e27902ce47
allanjie <allanmcgrady@gmail.com>
2017-01-05 10:15:22 +0800

    delete useless packags

--- src/com/statnlp/projects/dep/model/labelleddp/LDPConfig.java
+++
@@ -1,9 +0,0 @@
-package com.statnlp.projects.dep.model.labelleddp;
-
-public class LDPConfig {
-
-	
-	public static String COMPLABEL = "complete";
-	
-	public static String rootDepLabel = "sentence";
-}
--- src/com/statnlp/projects/dep/model/labner/LNERConfig.java
+++
@@ -1,9 +0,0 @@
-package com.statnlp.projects.dep.model.labner;
-
-public class LNERConfig {
-
-	
-	public static String COMPLABEL = "complete";
-	
-	public static String rootDepLabel = "sentence";
-}
