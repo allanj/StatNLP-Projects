3f540b9da4ed15d6c1fbeae4c2a966330ad2bf38
allanjie <allanmcgrady@gmail.com>
2017-01-20 16:27:57 +0800

    fix an issue in neural embedding configuration

--- src/com/statnlp/neural/NNCRFGlobalNetworkParam.java
+++ src/com/statnlp/neural/NNCRFGlobalNetworkParam.java
@@ -202,7 +202,7 @@ public class NNCRFGlobalNetworkParam extends NNCRFInterface {
 								fieldMap.put(elements[j], fieldIdx);
 								inputDimList.set(i, inputDimList.get(i)+1);
 								if (NeuralConfig.EMBEDDING.get(i).equals("glove")
-								|| NeuralConfig.EMBEDDING.get(i).equals("polyglot")) {
+								|| NeuralConfig.EMBEDDING.get(i).equals("polyglot") || NeuralConfig.EMBEDDING.get(i).equals("bansal") ) {
 									wordList.add(elements[j]);
 								}
 							}
